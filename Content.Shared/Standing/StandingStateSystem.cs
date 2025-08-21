5f2a11bdcebefa2eb7b3303efd99e05862eda86c
Tirochora <leotabletdb@gmail.com>
2025-08-20 21:29:38 -0400

    why did it add that using

--- Content.Shared/Standing/StandingStateSystem.cs
+++ Content.Shared/Standing/StandingStateSystem.cs
@@ -4,7 +4,6 @@ using Content.Shared.Hands.Components;
 using Content.Shared.Movement.Systems;
 using Content.Shared.Physics;
 using Content.Shared.Rotation;
-using Robust.Shared.Animations;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Physics;
 using Robust.Shared.Physics.Systems;
