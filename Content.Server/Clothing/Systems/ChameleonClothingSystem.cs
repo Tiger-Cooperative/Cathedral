95ba37cff6c442a284cffae0929aa4bba6f81aed
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-10-04 13:24:42 +0200

    Predict EMPs (#39802)
    * predicted emps
    
    * fixes
    
    * fix
    
    * review

--- Content.Server/Clothing/Systems/ChameleonClothingSystem.cs
+++ Content.Server/Clothing/Systems/ChameleonClothingSystem.cs
@@ -1,14 +1,10 @@
-using System.Linq;
-using Content.Server.Emp;
 using Content.Shared.Clothing.Components;
 using Content.Shared.Clothing.EntitySystems;
 using Content.Shared.Emp;
 using Content.Shared.IdentityManagement;
 using Content.Shared.IdentityManagement.Components;
-using Content.Shared.Inventory;
 using Content.Shared.Prototypes;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Random;
 
 namespace Content.Server.Clothing.Systems;
 
@@ -16,15 +12,12 @@ public sealed class ChameleonClothingSystem : SharedChameleonClothingSystem
 {
     [Dependency] private readonly IPrototypeManager _proto = default!;
     [Dependency] private readonly IdentitySystem _identity = default!;
-    [Dependency] private readonly IRobustRandom _random = default!;
 
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<ChameleonClothingComponent, MapInitEvent>(OnMapInit);
         SubscribeLocalEvent<ChameleonClothingComponent, ChameleonPrototypeSelectedMessage>(OnSelected);
-
-        SubscribeLocalEvent<ChameleonClothingComponent, EmpPulseEvent>(OnEmpPulse);
     }
 
     private void OnMapInit(EntityUid uid, ChameleonClothingComponent component, MapInitEvent args)
@@ -35,21 +28,6 @@ public sealed class ChameleonClothingSystem : SharedChameleonClothingSystem
     private void OnSelected(EntityUid uid, ChameleonClothingComponent component, ChameleonPrototypeSelectedMessage args)
     {
         SetSelectedPrototype(uid, args.SelectedId, component: component);
-    }
-
-    private void OnEmpPulse(EntityUid uid, ChameleonClothingComponent component, ref EmpPulseEvent args)
-    {
-        if (!component.AffectedByEmp)
-            return;
-
-        if (component.EmpContinuous)
-            component.NextEmpChange = Timing.CurTime + TimeSpan.FromSeconds(1f / component.EmpChangeIntensity);
-
-        var pick = GetRandomValidPrototype(component.Slot, component.RequireTag);
-        SetSelectedPrototype(uid, pick, component: component);
-
-        args.Affected = true;
-        args.Disabled = true;
     }
 
     private void UpdateUi(EntityUid uid, ChameleonClothingComponent? component = null)
@@ -64,7 +42,7 @@ public sealed class ChameleonClothingSystem : SharedChameleonClothingSystem
     /// <summary>
     ///     Change chameleon items name, description and sprite to mimic other entity prototype.
     /// </summary>
-    public void SetSelectedPrototype(EntityUid uid, string? protoId, bool forceUpdate = false,
+    public override void SetSelectedPrototype(EntityUid uid, string? protoId, bool forceUpdate = false,
         ChameleonClothingComponent? component = null)
     {
         if (!Resolve(uid, ref component, false))
@@ -86,14 +64,6 @@ public sealed class ChameleonClothingSystem : SharedChameleonClothingSystem
         UpdateVisuals(uid, component);
         UpdateUi(uid, component);
         Dirty(uid, component);
-    }
-
-    /// <summary>
-    ///     Get a random prototype for a given slot.
-    /// </summary>
-    public string GetRandomValidPrototype(SlotFlags slot, string? tag = null)
-    {
-        return _random.Pick(GetValidTargets(slot, tag).ToList());
     }
 
     public override void Update(float frameTime)
