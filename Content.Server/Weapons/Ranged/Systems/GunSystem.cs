c7d7f3869e3a4ac01c4fdbf11a58342ba7963d4b
Tirochora <leotabletdb@gmail.com>
2025-12-06 22:33:47 -0500

    fix gunSystem

--- Content.Server/Weapons/Ranged/Systems/GunSystem.cs
+++ Content.Server/Weapons/Ranged/Systems/GunSystem.cs
@@ -17,6 +17,7 @@ using Robust.Shared.Map;
 using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
+using Content.Shared.Hands.EntitySystems;
 
 namespace Content.Server.Weapons.Ranged.Systems;
 
